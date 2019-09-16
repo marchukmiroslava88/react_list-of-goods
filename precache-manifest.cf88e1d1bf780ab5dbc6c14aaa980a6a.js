b8181c8a7a3c39bde78bb4602d6e30b5f7e58533
Miroslava <777shadowpriest777@gmail.com>
2019-09-16 23:41:11 +0300

    Updates

---
+++ precache-manifest.cf88e1d1bf780ab5dbc6c14aaa980a6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efc2ea8a8b9fc23e1caeb75ca069cdf8",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "3d896d7f2f62599d6af0",
+    "url": "/react_list-of-goods/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3d896d7f2f62599d6af0",
+    "url": "/react_list-of-goods/static/js/2.0b1cbabb.chunk.js"
+  },
+  {
+    "revision": "af21843852113fa6e3d4",
+    "url": "/react_list-of-goods/static/js/main.e34e82ca.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
